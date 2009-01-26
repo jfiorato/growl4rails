14cddd91769c679cc2e434e55c706106b657a164
Jim Fiorato <jimfiorato@jim-fioratos-macbook-pro-2.local>
2009-01-26 15:12:53 -0600

    Update display issue when page is scrolled and growl appears.

--- public/javascripts/growl4rails.js
+++ public/javascripts/growl4rails.js
@@ -22,11 +22,11 @@ var templateHTML = '<div id="#{id}" class="growl4rails_cell" style="display:none
     </tr>\
   </thead>\
 </table>\
-<div id="growl4rails_info_#{id}" class="growl4rails_info">\
+</div>\
+<div id="growl4rails_info_#{id}" class="growl4rails_info" style="display:none;">\
   <img src="#{img}" class="growl4rails_image" align="left" />\
   <div class="growl4rails_title">#{title}</div>\
   <div class="growl4rails_message">#{message}<div>\
-</div>\
 </div>';
 
 var growl4rails_template = new Template(templateHTML);
@@ -40,7 +40,6 @@ var growl4rails_timer_hash = new Hash();
 var Growl4Rails = Class.create();
 
 Growl4Rails.showGrowl = function(arguments) {
-  
   //if we're not showing maximum number of growls then show it, otherwise queue it up
   if(growl4rails_current_showing >= growl4rails_max_showing)
     growl4rails_limit_reached = true;
@@ -67,32 +66,39 @@ Growl4Rails.showGrowl = function(arguments) {
     Element.insert(document.body, growl4rails_template.evaluate(data));
     
     //position the growl
-    $(growl_cell_id).style.left = (document.viewport.getWidth() - 400) + 'px';
+    var top_offset = 0;
+    var left_offset = document.viewport.getWidth() - 400;
+    $(growl_cell_id).style.left = left_offset + 'px';
     
     if(other_growls.length > 0) {
       var top_position = 0;
       other_growls.each(function(growl) {
         top_position += growl.getHeight();
       });
-      $(growl_cell_id).style.top = (document.viewport.getScrollOffsets().top + top_position + 10) + 'px';
+      top_offset = document.viewport.getScrollOffsets().top + top_position + 10;
     } else {
-      $(growl_cell_id).style.top = (document.viewport.getScrollOffsets().top + 10) + 'px';
+      top_offset = document.viewport.getScrollOffsets().top + 10;
     }
+    $(growl_cell_id).style.top = top_offset + 'px';
     
     //position the text and image
-    $('growl4rails_info_' + growl_cell_id).style.top = ($(growl_cell_id).viewportOffset().top + 28) + 'px';
-    $('growl4rails_info_' + growl_cell_id).style.left = 28 + 'px';
+    $('growl4rails_info_' + growl_cell_id).style.top = (top_offset + 28) + 'px';
+    $('growl4rails_info_' + growl_cell_id).style.left = left_offset + 28 + 'px';
   
     //wire up the growl mouse events
     $(growl_cell_id).observe('mouseover', Growl4Rails.mouseOver);
     $(growl_cell_id).observe('mouseout', Growl4Rails.mouseOut);
     $(growl_cell_id).observe('click', Growl4Rails.click);
+    $('growl4rails_info_' + growl_cell_id).observe('click', Growl4Rails.click);
     
     //Temporary until we've got better support for opacity and alpha transparency
-    if(Prototype.Browser.IE)
+    if(Prototype.Browser.IE) {
+      $('growl4rails_info_' + growl_cell_id).show();
       $(growl_cell_id).show();
-    else
+    } else {  
+      Effect.Appear('growl4rails_info_' + growl_cell_id);
       Effect.Appear(growl_cell_id);
+    }
     
     $('growl4rails_body_' + growl_cell_id).style.height = ($('growl4rails_info_' + growl_cell_id).getDimensionsPatched().height - 20) + 'px';
     
@@ -112,10 +118,13 @@ Growl4Rails.showGrowl = function(arguments) {
 
 Growl4Rails.hideGrowl = function(growl_cell_id) {
   growl4rails_timer_hash.unset(growl_cell_id);
-  if(Prototype.Browser.IE) 
+  if(Prototype.Browser.IE) {
+    $('growl4rails_info_' + growl_cell_id).hide();
     $(growl_cell_id).hide();
-  else
+  } else {
+    Effect.Fade('growl4rails_info_' + growl_cell_id, { duration: 1.0 });
     Effect.Fade(growl_cell_id, { duration: 1.0 });
+  }
     
   setTimeout("Growl4Rails.removeGrowl('" + growl_cell_id + "')", 1000);
 };
@@ -137,9 +146,9 @@ var mouseOverClasses = ['growl4rails_corner_ul', 'growl4rails_top', 'growl4rails
 Growl4Rails.mouseOver = function(event) {
   //bunch of hoo-ha to make sure we're not handling bubbled mouseover events.
   var relatedTarget = event.relatedTarget;
-  var currentTarget = event.currentTarget ? event.currentTarget : event.srcElement;           
+  var currentTarget = event.currentTarget ? event.currentTarget : event.srcElement;
   if (relatedTarget && relatedTarget.nodeType == Node.TEXT_NODE) relatedTarget = relatedTarget.parentNode;   
-  if (relatedTarget && relatedTarget != currentTarget && !Element.descendantOf(relatedTarget, currentTarget)) {
+  if (relatedTarget && relatedTarget != currentTarget && !Element.descendantOf(relatedTarget, currentTarget) && !relatedTarget.ancestors().any(function(n) { return n.hasClassName('growl4rails_info'); })) {
     growl_cell = Growl4Rails.findGrowlIdByDescendant(Event.findElement(event));
     mouseOverClasses.each(function(item) {
      $$('#' + growl_cell.id + ' .' + item)[0].toggleClassName(item + '_hi');
@@ -152,9 +161,9 @@ Growl4Rails.mouseOver = function(event) {
 Growl4Rails.mouseOut = function(event) {
   //bunch of hoo-ha to make sure we're not handling bubbled mouseover events.
   var relatedTarget = event.relatedTarget;
-  var currentTarget = event.currentTarget ? event.currentTarget : event.srcElement;           
+  var currentTarget = event.currentTarget ? event.currentTarget : event.srcElement;
   if (relatedTarget && relatedTarget.nodeType == Node.TEXT_NODE) relatedTarget = relatedTarget.parentNode;   
-  if (relatedTarget && relatedTarget != currentTarget && !Element.descendantOf(relatedTarget, currentTarget)) {
+  if (relatedTarget && relatedTarget != currentTarget && !Element.descendantOf(relatedTarget, currentTarget) && !relatedTarget.ancestors().any(function(n) { return n.hasClassName('growl4rails_info'); })) {
     growl_cell = Growl4Rails.findGrowlIdByDescendant(Event.findElement(event));
     mouseOverClasses.each(function(item) {
      $$('#' + growl_cell.id + ' .' + item)[0].toggleClassName(item + '_hi');
@@ -175,11 +184,13 @@ Growl4Rails.findGrowlIdByDescendant = function(descendant) {
   if(descendant.className == 'growl4rails_cell')
     return descendant;
     
-  var growl_cell;
-  descendant.ancestors().each(function(ancestor){
-    if(ancestor.className == 'growl4rails_cell')
-      growl_cell = ancestor;
-  });
+  var growl_cell = descendant.ancestors().find(function(ancestor){ return ancestor.hasClassName('growl4rails_cell'); });
+  
+  if(!growl_cell) {
+    growl_info = descendant.ancestors().find(function(ancestor){ return ancestor.hasClassName('growl4rails_info'); });
+    if(growl_info)
+      growl_cell = $(growl_info.id.sub('growl4rails_info_', ''));
+  }
   return growl_cell
 };
 
